b223739314d225639b0ff4f967af97b9ebe9ca93
Burak Uyar <burakuyar25@gmail.com>
2018-01-14 21:41:15 +0300

    Service has implemented for User model class

---
+++ src/main/java/service/UserService.java
@@ -0,0 +1,17 @@
+package service;
+
+import java.util.List;
+
+import model.User;
+
+public interface UserService {
+	long save(User user);
+
+	User get(long id);
+
+	List<User> list();
+
+	void update(long id, User user);
+
+	void delete(long id);
+}
---
+++ src/main/java/service/UserServiceImp.java
@@ -0,0 +1,42 @@
+package service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import dao.UserDao;
+import model.User;
+
+@Service
+@Transactional(readOnly = true)
+public class UserServiceImp implements UserService {
+
+	@Autowired
+	UserDao userDao;
+
+	@Transactional
+	public long save(User user) {
+		return userDao.save(user);
+	}
+
+	public User get(long id) {
+		return userDao.get(id);
+	}
+
+	public List<User> list() {
+		return userDao.list();
+	}
+
+	@Transactional
+	public void update(long id, User user) {
+		userDao.update(id, user);
+
+	}
+
+	@Transactional
+	public void delete(long id) {
+		userDao.delete(id);
+	}
+}
