230ff0daf0f1223ff27955a7d4f666a1d5ef3a07
Burak Uyar <burakuyar25@gmail.com>
2018-02-02 14:28:06 +0300

    Jackson has used in Topic controller and fixed the enroll functionality

--- src/main/java/controller/TopicController.java
+++ src/main/java/controller/TopicController.java
@@ -1,13 +1,23 @@
 package controller;
 
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import model.SubTopic;
+import model.Talent;
 import model.Topic;
+import model.User;
+import model.UserSubTopicId;
 
+import org.hibernate.exception.ConstraintViolationException;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,73 +37,51 @@ public class TopicController {
     private UserService userService;
 
     @GetMapping("/topic")
-    public ResponseEntity<String> list() {
-        JSONObject response = new JSONObject();
-        JSONArray topics = new JSONArray();
-        response.put("topics", topics);
-        for (Topic topic : topicService.list()) {
-            JSONObject obj = new JSONObject();
-            obj.put("id", topic.getId());
-            obj.put("title", topic.getTitle());
-            obj.put("createDate", topic.getCreateDate());
-            obj.put("enrolledNumber", topic.getEnrolledNumber());
-            obj.put("waitingToGrouped", topic.getWaitingToGrouped());
-            obj.put("totalGroupNumber", topic.getTotalGroupNumber());
-            obj.put("nextGroupingTime", topic.getNextGroupingTime());
-            topics.put(obj);
-        }
-        return ResponseEntity.ok().body(response.toString());
+    public ResponseEntity<List<Topic>> list() {
+        return ResponseEntity.ok().body(topicService.list());
     }
-    
+
     @PostMapping("/topic")
-    public ResponseEntity<?> save(@RequestBody String str) {
+    public ResponseEntity<?> save(@RequestBody Topic topic) {
         Long userId = 1l; // TODO get userId from token
-        JSONObject obj = new JSONObject(str);
-        Topic topic = new Topic();
-        topic.setTitle(obj.getString("title"));
-        topic.setMinSize(obj.getInt("minSize"));
-        topic.setMaxSize(obj.getInt("maxSize"));
         topic.setCreator(userService.get(userId));
-        JSONArray subTopicsArr = obj.getJSONArray("subTopics");
-        Set<SubTopic> subTopics = topic.getSubTopics();
-        for (int i = 0; i < subTopicsArr.length(); i++) {
-            SubTopic subTopic = new SubTopic();
+        for (SubTopic subTopic : topic.getSubTopics()) {
             subTopic.setTopic(topic);
-            subTopic.setTitle(subTopicsArr.getJSONObject(i).getString("title"));
-            subTopics.add(subTopic);
         }
-        topic.setSubTopics(subTopics);
         long id = topicService.save(topic);
         return ResponseEntity.ok().body("New topic has been saved with ID:" + id);
     }
 
     @GetMapping("/topic/{id}")
-    public ResponseEntity<String> get(@PathVariable("id") long id) {
-        Topic topic = topicService.get(id);
-        JSONObject response = new JSONObject();
-        response.put("id", topic.getId());
-        response.put("title", topic.getTitle());
-        response.put("createDate", topic.getCreateDate());
-        response.put("enrolledNumber", topic.getEnrolledNumber());
-        response.put("waitingToGrouped", topic.getWaitingToGrouped());
-        response.put("totalGroupNumber", topic.getTotalGroupNumber());
-        response.put("nextGroupingTime", topic.getNextGroupingTime());
-        JSONArray subTopics = new JSONArray();
-        response.put("subTopics", subTopics);
-        for (SubTopic subTopic : topic.getSubTopics()) {
-            JSONObject objSubTopic = new JSONObject();
-            objSubTopic.put("id", subTopic.getId());
-            objSubTopic.put("title", subTopic.getTitle());
-            subTopics.put(objSubTopic);
-        }
-        return ResponseEntity.ok().body(response.toString());
+    public ResponseEntity<Topic> get(@PathVariable("id") long id) {
+        return ResponseEntity.ok().body(topicService.get(id));
     }
 
-    @PostMapping("/topic/{topicId}")
-    public ResponseEntity<?> enroll(@PathVariable("") long topicID, @RequestHeader("X-Auth") String token) {
-        throw new UnsupportedOperationException("Not supported yet.");
-//        long userId = 0;
-//        topicService.enroll(topicID, userId);
-//        return ResponseEntity.ok().body("User has enrolled to the topic successfully.");
+    @PostMapping("/topic/{id}")
+    public ResponseEntity<?> enroll(@PathVariable("id") long topicId, @RequestBody String strTalents, @RequestHeader("X-Auth") String token) {
+        // throw new UnsupportedOperationException("Not supported yet.");
+        Long userId = 1l; // TODO get userId from token
+        User user = userService.get(userId);
+        Topic topic = topicService.get(topicId);
+        Map<Long, SubTopic> subTopicsMap = new HashMap<>();
+        for (SubTopic subTopic : topic.getSubTopics()) {
+            subTopicsMap.put(subTopic.getId(), subTopic);
+        }
+        JSONObject rootObj = new JSONObject(strTalents);
+        JSONArray arrTalents = rootObj.getJSONArray("talents");
+        for (int i = 0; i < arrTalents.length(); i++) {
+            JSONObject talentObj = arrTalents.getJSONObject(i);
+            Talent talent = new Talent();
+            talent.setScore(talentObj.getInt("score"));
+            UserSubTopicId userSubTopicId = new UserSubTopicId(user, subTopicsMap.get(talentObj.getLong("subTopic"))); // TODO: After SubTopic Service is ready, fix it
+            talent.setUserSubTopicId(userSubTopicId);
+            user.getTalents().add(talent);
+        }
+        try {
+            topicService.enroll(topic, user);
+            return ResponseEntity.ok().body("User has enrolled to the topic successfully.");
+        } catch (ConstraintViolationException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("User has already enrolled.");
+        }
     }
 }
