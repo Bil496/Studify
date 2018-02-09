03d568772e77db7bb5781d13e5abb070d229a268
Görkem Mülayim <gorkemmulayim@icloud.com>
2018-02-09 19:50:29 +0300

    Fixed the exception thrown by list method of class Topic.

--- src/main/java/controller/TopicController.java
+++ src/main/java/controller/TopicController.java
@@ -1,32 +1,21 @@
 package controller;
 
-import java.sql.SQLIntegrityConstraintViolationException;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
-import model.SubTopic;
-import model.Talent;
-import model.Topic;
-import model.User;
-import model.UserSubTopicId;
-
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import model.*;
 import org.hibernate.exception.ConstraintViolationException;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import service.TopicService;
 import service.UserService;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class TopicController {
@@ -37,8 +26,10 @@ public class TopicController {
     private UserService userService;
 
     @GetMapping("/topic")
-    public ResponseEntity<List<Topic>> list() {
-        return ResponseEntity.ok().body(topicService.list());
+    public ResponseEntity<String> list() throws JsonProcessingException {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        String json = ow.writeValueAsString(topicService.list());
+        return ResponseEntity.ok().body(json);
     }
 
     @PostMapping("/topic")
