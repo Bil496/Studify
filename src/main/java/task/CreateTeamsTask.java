bf28373564c37e81f9d83dd26d0f6421267bc84c
Burak Uyar <burakuyar25@gmail.com>
2018-02-22 23:49:33 +0300

    Task takes topic id

--- src/main/java/task/CreateTeamsTask.java
+++ src/main/java/task/CreateTeamsTask.java
@@ -4,11 +4,16 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class CreateTeamsTask implements Runnable {
+    private long topicId;
 
+    public CreateTeamsTask(long topicId) {
+        this.topicId = topicId;
+    }
+    
     @Override
     public void run() {
-	//System.out.println(System.currentTimeMillis());
-	// Implement Studfy Algorithm here...
+        //System.out.println(System.currentTimeMillis());
+        // Implement Studfy Algorithm here...
     }
 
 }
