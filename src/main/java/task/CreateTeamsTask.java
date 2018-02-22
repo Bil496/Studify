e30dd836fb74f4e031bf1024e0527b296e0be9e4
Burak Uyar <burakuyar25@gmail.com>
2018-02-22 23:58:37 +0300

    Prepared all needed datas for team creating task.

--- src/main/java/task/CreateTeamsTask.java
+++ src/main/java/task/CreateTeamsTask.java
@@ -1,10 +1,25 @@
 package task;
 
+import model.Topic;
+import model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import service.TalentService;
+import service.TopicService;
+import service.UserService;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Component
 public class CreateTeamsTask implements Runnable {
     private long topicId;
+    @Autowired
+    TopicService topicService;
+    @Autowired
+    UserService userService;
+    @Autowired
+    TalentService talentService;
 
     public CreateTeamsTask(long topicId) {
         this.topicId = topicId;
@@ -12,7 +27,16 @@ public class CreateTeamsTask implements Runnable {
     
     @Override
     public void run() {
-        //System.out.println(System.currentTimeMillis());
+        Topic topic = topicService.get(topicId);
+        Set<User> usersWithoutTeam = topicService.getUsersWithoutTeam(topic);
+        for (User user : usersWithoutTeam){
+            user.setTalents(new HashSet<>(talentService.getTalentsByTopicId(user.getId(), topic.getId())));
+        }
+        /*
+            usersWithoutTeam has users
+            WARN: users only have ID and talents field, other fields are empty.
+            In case their other fields are needed, feel free to call userService.get(user.getId()) method for each user.
+         */
         // Implement Studfy Algorithm here...
     }
 
