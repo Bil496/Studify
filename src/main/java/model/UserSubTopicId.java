f4c0a136c13a31d4c0b7e19077f01f0aa19110e2
Görkem Mülayim <gorkemmulayim@icloud.com>
2018-02-24 14:41:33 +0300

    Added default constructor to fix an error in runtime."

--- src/main/java/model/UserSubTopicId.java
+++ src/main/java/model/UserSubTopicId.java
@@ -2,35 +2,39 @@ package model;
 
 import javax.persistence.Embeddable;
 import javax.persistence.ManyToOne;
+import java.io.Serializable;
 
 @SuppressWarnings("serial")
 @Embeddable
-public class UserSubTopicId implements java.io.Serializable {
+public class UserSubTopicId implements Serializable {
     @ManyToOne
     private User user;
 
     @ManyToOne
     private SubTopic subTopic;
 
+    public UserSubTopicId() {
+    }
+
     public UserSubTopicId(User user, SubTopic subTopic) {
-	super();
-	this.user = user;
-	this.subTopic = subTopic;
+        super();
+        this.user = user;
+        this.subTopic = subTopic;
     }
 
     public User getUser() {
-	return user;
+        return user;
     }
 
     public void setUser(User user) {
-	this.user = user;
+        this.user = user;
     }
 
     public SubTopic getSubTopic() {
-	return subTopic;
+        return subTopic;
     }
 
     public void setSubTopic(SubTopic subTopic) {
-	this.subTopic = subTopic;
+        this.subTopic = subTopic;
     }
 }
