818d79f45a16b327610e79f0fe69c8be3e1e437d
ferdemKKaya <fatiherdemkizilkaya@gmail.com>
2018-02-26 22:35:25 +0300

    Created GreedyStrongNashEquilibriumAlgorithm class that extends MatchingAlgorithm for implementation of the algorithm which matches the users such that teams returned by the algorithm is a strong Nash equilibrium according to talent based group formation games.

---
+++ src/main/java/algorithm/GreedyStrongNashEquilibriumAlgorithm.java
@@ -0,0 +1,20 @@
+package algorithm;
+
+import java.util.List;
+
+import model.Team;
+import model.Topic;
+import model.User;
+
+public class GreedyStrongNashEquilibriumAlgorithm extends MatchingAlgorithm {
+
+    public GreedyStrongNashEquilibriumAlgorithm(Topic topic, List<User> users) {
+	super(topic, users);
+    }
+
+    @Override
+    public List<Team> match() {
+	return null;
+    }
+
+}
