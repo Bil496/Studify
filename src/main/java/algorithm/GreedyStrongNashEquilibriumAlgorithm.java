fd904f71c951e7739d2ae86d1eb89b738758f978
ferdemKKaya <fatiherdemkizilkaya@gmail.com>
2018-02-26 23:39:45 +0300

    Added Candidate inner class for candidates teams during the algorithm.

--- src/main/java/algorithm/GreedyStrongNashEquilibriumAlgorithm.java
+++ src/main/java/algorithm/GreedyStrongNashEquilibriumAlgorithm.java
@@ -11,6 +11,42 @@ public class GreedyStrongNashEquilibriumAlgorithm extends MatchingAlgorithm {
     public GreedyStrongNashEquilibriumAlgorithm(Topic topic, List<User> users) {
 	super(topic, users);
     }
+    
+    private class Candidate {
+
+	private int[] members;
+	private int jointUtility;
+
+	public Candidate(int[] members) {
+	    this.members = members;
+
+	    // Initialize joint talent vector
+	    Integer[] jointTalent = new Integer[topic.getMaxSize()];
+	    for (int i = 0; i < topic.getMaxSize(); i++) {
+		jointTalent[i] = 0;
+	    }
+
+	    // Calculate joint talent vector
+	    for (int i : members) {
+		for (int j = 0; j < topic.getMaxSize(); j++) {
+		    User user = users.get(i);
+		    //List<Talent> talents = user.getTalents();
+		    int[] talents = user.getTalents().stream().mapToInt(t -> t.getScore()).toArray();
+		    
+		    if (talents[i] > jointTalent[j]) {
+			jointTalent[j] = talents[i]; 
+		    }
+		}
+	    }
+
+	    // Calculate joint utility
+	    jointUtility = 0;
+	    for (int i = 0; i < topic.getMaxSize(); i++) {
+		jointUtility += jointTalent[i];
+	    }
+	}
+
+    }
 
     @Override
     public List<Team> match() {
