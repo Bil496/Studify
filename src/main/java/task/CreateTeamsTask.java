f3a3c8344b96fa3ca01b8a8cb4d1d565c7ba849a
ferdemKKaya <fatiherdemkizilkaya@gmail.com>
2018-02-27 00:50:46 +0300

    Changed dummy algorithm with greedy strong nash equilibrium algorithm.

--- src/main/java/task/CreateTeamsTask.java
+++ src/main/java/task/CreateTeamsTask.java
@@ -3,14 +3,14 @@ package task;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
-import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import algorithm.BasicAlgorithm;
-import algorithm.TeamCreateAlgorithm;
+import algorithm.GreedyStrongNashEquilibriumAlgorithm;
+import algorithm.MatchingAlgorithm;
 import model.Talent;
 import model.Team;
 import model.Topic;
@@ -35,7 +35,6 @@ public class CreateTeamsTask implements Runnable {
     
     @Override
     public void run() {
-        TeamCreateAlgorithm algorithm = new BasicAlgorithm();
         Topic topic = topicService.get(topicId);
         List<User> usersWithoutTeam = topicService.getUsersWithoutTeam(topic);
         for (User user : usersWithoutTeam) {
@@ -44,7 +43,8 @@ public class CreateTeamsTask implements Runnable {
             Collections.sort(talents);
             user.setTalents(talents);
         }
-        List<Team> teams = algorithm.createTeams(topic, usersWithoutTeam);
+        MatchingAlgorithm algorithm = new GreedyStrongNashEquilibriumAlgorithm(topic, usersWithoutTeam);
+        Set<Team> teams = algorithm.match();
         topic.setWaitingToGrouped(usersWithoutTeam.size() - getTotalUsersInTeams(teams));
         topic.setTotalGroupNumber(topic.getTotalGroupNumber() + teams.size());
         topic.setNextGroupingTime(new Date(topic.getNextGroupingTime().getTime() + (1000 * 60 * 60 * 24)));
@@ -54,10 +54,9 @@ public class CreateTeamsTask implements Runnable {
             WARN: users only have ID and talents field, other fields are empty.
             In case their other fields are needed, feel free to call userService.get(user.getId()) method for each user.
          */
-        // Implement Studfy Algorithm to algorithm package with implementing TeamCreateAlgorithm interface.
     }
 
-    private int getTotalUsersInTeams(List<Team> teams){
+    private int getTotalUsersInTeams(Set<Team> teams){
         int total = 0;
         for (Team team: teams){
             total += team.getSize();
