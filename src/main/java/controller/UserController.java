f20bc79ab119054c3f3cfcf31128426229c0b79a
Görkem Mülayim <gorkemmulayim@icloud.com>
2018-02-27 12:22:27 +0300

    Fixed indendations.

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,21 +1,14 @@
 package controller;
 
-import java.util.List;
-
 import model.Team;
+import model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
-import model.User;
+import org.springframework.web.bind.annotation.*;
 import service.TeamService;
 import service.UserService;
+
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -28,22 +21,22 @@ public class UserController {
     /*---Add new user---*/
     @PostMapping("/user")
     public ResponseEntity<?> save(@RequestBody User user) {
-	long id = userService.save(user);
-	return ResponseEntity.ok().body("New User has been saved with ID:" + id);
+        long id = userService.save(user);
+        return ResponseEntity.ok().body("New User has been saved with ID:" + id);
     }
 
     /*---Get a user by id---*/
     @GetMapping("/user/{id}")
     public ResponseEntity<User> get(@PathVariable("id") long id) {
-	User user = userService.get(id);
-	return ResponseEntity.ok().body(user);
+        User user = userService.get(id);
+        return ResponseEntity.ok().body(user);
     }
 
     /*---get all books---*/
     @GetMapping("/user")
     public ResponseEntity<List<User>> list() {
-	List<User> users = userService.list();
-	return ResponseEntity.ok().body(users);
+        List<User> users = userService.list();
+        return ResponseEntity.ok().body(users);
     }
 
     /*---get all teams of user---*/
@@ -56,14 +49,14 @@ public class UserController {
     /*---Update a user by id---*/
     @PutMapping("/user/{id}")
     public ResponseEntity<?> update(@PathVariable("id") long id, @RequestBody User user) {
-	userService.update(id, user);
-	return ResponseEntity.ok().body("User has been updated successfully.");
+        userService.update(id, user);
+        return ResponseEntity.ok().body("User has been updated successfully.");
     }
 
     /*---Delete a user by id---*/
     @DeleteMapping("/user/{id}")
     public ResponseEntity<?> delete(@PathVariable("id") long id) {
-	userService.delete(id);
-	return ResponseEntity.ok().body("User has been deleted successfully.");
+        userService.delete(id);
+        return ResponseEntity.ok().body("User has been deleted successfully.");
     }
 }
--- src/main/java/dao/UserDaoImp.java
+++ src/main/java/dao/UserDaoImp.java
@@ -1,11 +1,6 @@
 package dao;
 
-import java.util.List;
-
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
-
+import model.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
@@ -13,7 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Repository;
 
-import model.User;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
 
 @Repository
 public class UserDaoImp implements UserDao {
@@ -23,46 +21,39 @@ public class UserDaoImp implements UserDao {
     private PasswordEncoder passwordEncoder;
 
     public long save(User user) {
-	user.setPassword(passwordEncoder.encode(user.getPassword()));
-	sessionFactory.getCurrentSession().save(user);
-	return user.getId();
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        sessionFactory.getCurrentSession().save(user);
+        return user.getId();
     }
 
     public User get(long id) {
-	return sessionFactory.getCurrentSession().get(User.class, id);
+        return sessionFactory.getCurrentSession().get(User.class, id);
     }
 
     public List<User> list() {
-	Session session = sessionFactory.getCurrentSession();
-	CriteriaBuilder cb = session.getCriteriaBuilder();
-	CriteriaQuery<User> cq = cb.createQuery(User.class);
-	Root<User> root = cq.from(User.class);
-	cq.select(root);
-	Query<User> query = session.createQuery(cq);
-	return query.getResultList();
+        Session session = sessionFactory.getCurrentSession();
+        CriteriaBuilder cb = session.getCriteriaBuilder();
+        CriteriaQuery<User> cq = cb.createQuery(User.class);
+        Root<User> root = cq.from(User.class);
+        cq.select(root);
+        Query<User> query = session.createQuery(cq);
+        return query.getResultList();
     }
 
     public void update(long id, User user) {
-	Session session = sessionFactory.getCurrentSession();
-	User user2 = session.byId(User.class).load(id);
-	user2.setName(user.getName());
-	user2.setSurname(user.getSurname());
-	user2.setEmail(user.getEmail());
-	user2.setUsername(user.getUsername());
-	user2.setPassword(passwordEncoder.encode(user.getPassword()));
-	session.flush();
+        Session session = sessionFactory.getCurrentSession();
+        User user2 = session.byId(User.class).load(id);
+        user2.setName(user.getName());
+        user2.setSurname(user.getSurname());
+        user2.setEmail(user.getEmail());
+        user2.setUsername(user.getUsername());
+        user2.setPassword(passwordEncoder.encode(user.getPassword()));
+        session.flush();
     }
 
     public void delete(long id) {
-	Session session = sessionFactory.getCurrentSession();
-	User user = session.byId(User.class).load(id);
-	session.delete(user);
+        Session session = sessionFactory.getCurrentSession();
+        User user = session.byId(User.class).load(id);
+        session.delete(user);
     }
-
-    /*
-     * public Boolean login(long id, User user) { Session session =
-     * sessionFactory.getCurrentSession(); User user2 =
-     * session.byId(User.class).load(id); return
-     * passwordEncoder.matches(user.getPassword(), user2.getPassword()); }
-     */
 }
