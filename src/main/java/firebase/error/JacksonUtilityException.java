4027e5eee0b146cd14e9fb6585fa0c961728b1b9
Ahmet Selim Kaya <askaya@etu.edu.tr>
2018-03-13 00:18:51 +0300

    Exception classes added.

---
+++ src/main/java/firebase/error/JacksonUtilityException.java
@@ -0,0 +1,20 @@
+package firebase.error;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Created by ask on 13.03.2018
+ */
+public class JacksonUtilityException extends Throwable {
+    protected static final Logger LOGGER = Logger.getRootLogger();
+
+    private static final long serialVersionUID = 1L;
+
+    public JacksonUtilityException( String message ) {
+        super( message );
+    }
+
+    public JacksonUtilityException( String message, Throwable cause ) {
+        super( message, cause );
+    }
+}
