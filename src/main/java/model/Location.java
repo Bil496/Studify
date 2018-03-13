3df34da07a21140f15c29ff1d4a526efbbbb36f3
ferdemKKaya <fatiherdemkizilkaya@gmail.com>
2018-03-13 21:20:41 +0300

    Added new model Location.

---
+++ src/main/java/model/Location.java
@@ -0,0 +1,64 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public class Location {
+    
+    private Integer id;
+    private String title;
+    
+    @JsonIgnore
+    private List<Topic> topics = new ArrayList<>();
+
+    public Location() {
+    }
+
+    public Location(Integer id, String title, List<Topic> topics) {
+        this.id = id;
+        this.title = title;
+        this.topics = topics;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<Topic> getTopics() {
+        return topics;
+    }
+
+    public void setTopics(List<Topic> topics) {
+        this.topics = topics;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Location location = (Location) o;
+
+        return id.equals(location.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+
+}
